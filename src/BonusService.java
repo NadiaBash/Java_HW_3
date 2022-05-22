952cecdd2ddb39a3ad0ca53e6864541da59afa11
Nadia Bash <pretty_belka_2018@bk.ru>
2022-05-22 19:08:26 +0500

    Changed commit

--- src/BonusService.java
+++ src/BonusService.java
@@ -1,7 +1,7 @@
 public class BonusService {
-    public int calculate (int cost){
+    public int calculate(int cost) {
         int miles = 1;
-        int spentMoney =20;
+        int spentMoney = 20;
         int bonus = cost / spentMoney * miles;
         return bonus;
     }
